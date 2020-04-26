b160ceb08dcc35adbfd32d1686b4d224b96005e6
Aléfe Chorna <alefechorna99@gmail.com>
2020-04-26 02:38:22 -0300

    refactor: Add props to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,8 +4,12 @@ import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <Container type="submit">{children}</Container>;
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Container type="submit" {...rest}>
+      {children}
+    </Container>
+  );
 };
 
 export default Button;
