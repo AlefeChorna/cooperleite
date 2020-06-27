64672beb7ca7c2604c6872bcc25adc756e76c377
Aléfe Chorna <alefechorna99@gmail.com>
2020-06-26 23:02:03 -0300

    feat: Menu options on NavMenu component

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import ListItemText from '@material-ui/core/ListItemText';
+import CloneIcon from '@material-ui/icons/FileCopy';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import Buttom from '@material-ui/core/Button';
 
 import InputSearch from '../InputSearch';
 
-import { Container, Link } from './styles';
+import {
+  Container,
+  Link,
+  OptionsButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+} from './styles';
 
 const NavMenu: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  function handleOpenMenuOptions(event: any) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleCloseMenuOptions() {
+    setAnchorEl(null);
+  }
+
   return (
     <Container>
       <InputSearch
@@ -13,6 +35,40 @@ const NavMenu: React.FC = () => {
         containerStyle={{ marginRight: 25 }}
         iconColor="#fd951f"
       />
+
+      <OptionsButton
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="inherit"
+        onClick={handleOpenMenuOptions}
+        endIcon={<ArrowDropDownIcon style={{ fontSize: 24 }} />}
+      >
+        Opções
+      </OptionsButton>
+      <Menu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenuOptions}
+      >
+        <Link to="/EDIT">
+          <MenuItem onClick={handleCloseMenuOptions}>
+            <ListItemIcon>
+              <CloneIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText primary="Clonar" />
+          </MenuItem>
+        </Link>
+        <MenuItem onClick={() => {}}>
+          <ListItemIcon>
+            <DeleteIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Excluir" />
+        </MenuItem>
+      </Menu>
+
       <Link to="/animals/edit">
         <Buttom
           style={{
--- src/components/NavMenu/styles.ts
+++ src/components/NavMenu/styles.ts
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import MUMenu from '@material-ui/core/Menu';
+import MUListItemIcon from '@material-ui/core/ListItemIcon';
+import MUMenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Container = styled.div`
@@ -11,4 +15,65 @@ export const Container = styled.div`
 export const Link = styled(RouterLink)`
   text-decoration: none;
   color: inherit;
+`;
+
+export const OptionsButton = styled(Button)`
+  margin-right: 15px !important;
+  background-color: #29292e !important;
+  color: #fb3bc9 !important;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  margin-right: 10px;
+  border: 1px solid #555 !important;
+
+  &:hover {
+    background-color: rgb(251, 251, 251);
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const ListItemIcon = styled(MUListItemIcon)`
+  min-width: 36px !important;
+`;
+
+export const MenuItem = styled(MUMenuItem)`
+  &:focus {
+    background-color: #ff3bc9 !important;
+    & .MuiListItemIcon-root,
+    & .MuiListItemText-primary {
+      color: #fff;
+    }
+  }
+`;
+
+export const Menu = styled(MUMenu).attrs({
+  elevation: 0,
+  getContentAnchorEl: null,
+  anchorOrigin: {
+    vertical: 'bottom',
+    horizontal: 'center',
+  },
+  transformOrigin: {
+    vertical: 'top',
+    horizontal: 'center',
+  },
+})`
+  margin-top: 5px;
+  & .MuiMenu-paper {
+    background-color: #29292e;
+    box-shadow: 0 0 1px 0 rgba(0, 0, 0, 0.7), 0 5px 8px -2px rgba(0, 0, 0, 0.5);
+
+    ul {
+      color: #fff !important;
+      font-weight: 600 !important;
+
+      svg {
+        color: #fff;
+      }
+
+      li:hover {
+        background-color: #202020;
+      }
+    }
+  }
 `;
